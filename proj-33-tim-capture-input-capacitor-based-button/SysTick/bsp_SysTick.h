cbbb72eeb2584524599330fe0fb3aa033a03526c
Core <69836080+wsdron@users.noreply.github.com>
2026-08-26 23:00:54 -0700

    wsdron/proj 33 improvement (#20)
    - **add more debug info to the program, also add calibration check**
    - **manual linting**
    - **added clang format**
    - **added one more linting rule**

--- proj-33-tim-capture-input-capacitor-based-button/SysTick/bsp_SysTick.h
+++ proj-33-tim-capture-input-capacitor-based-button/SysTick/bsp_SysTick.h
@@ -5,10 +5,9 @@
 
 void SysTick_Init(void);
 void Delay_us(__IO u32 nTime);
-#define Delay_ms(x) Delay_us(1000*x)	 //单位ms
+#define Delay_ms(x) Delay_us(1000 * x) // 单位ms
 
-void SysTick_Delay_Us( __IO uint32_t us);
-void SysTick_Delay_Ms( __IO uint32_t ms);
-
+void SysTick_Delay_Us(__IO uint32_t us);
+void SysTick_Delay_Ms(__IO uint32_t ms);
 
 #endif /* __SYSTICK_H */
